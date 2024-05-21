abdd9e18908dbb43edeefeb075a529a671364f07
leduy1561993 <leduy1561993@gmail.com>
2024-05-21 22:57:31 +0700

    add

---
+++ analytics/config.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db_username = "myuser"
+db_password = "mypassword"
+db_host = "127.0.0.1"
+db_port = "5433"
+db_name = "postgres"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db = SQLAlchemy(app)
+
+app.logger.setLevel(logging.DEBUG)
