5af00db99784e8efdad53879e4db81a3e89caeca
leduy1561993 <leduy1561993@gmail.com>
2024-05-28 18:45:39 +0700

    add

--- analytics/app.py
+++ analytics/app.py
@@ -1,10 +1,14 @@
+import logging
 import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
 from flask import jsonify
-from sqlalchemy import text
+from sqlalchemy import and_, text
+from random import randint
 
 from config import app, db
+
 
 port_number = int(os.environ.get("APP_PORT", 5153))
 
